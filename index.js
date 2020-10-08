bca1384a22af7ad8c055dbd82a0f50b0621eee12
HooferDevelops <BatHun0214A@mischools.org>
2020-10-08 18:37:37 -0400

    lobby tests, images

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 // Classes
-var lobby = require("./objects/lobbytemplate.js")
+var Lobby = require("./objects/lobbytemplate.js")
+
+// tl;dr this is basically a holder for all of the 'servers'
+var Projects = {
+
+}
 
 // Connection Port
 http.listen(process.env.PORT || 3000);
 
-console.log(new lobby());
+console.log(new Lobby(null, "test lobby"));
 io.on('connection', (socket) => {
-
+    console.log(socket.request);
+    socket.on("createProjectInstance", (data)=>{
+        // check if data has a project name
+        // check if project name already exists in given context
+        // create a new project instance
+    })
 });
--- objects/lobbytemplate.js
+++ objects/lobbytemplate.js
@@ -1,13 +1,11 @@
 class Lobby {
     constructor(id, name) {
         this.id = id || Math.floor(Math.random()*999999999999);
-        this.name = name || "no name";
+        this.name = name;
     }
 
-    get id(){
-        return this.id;
-    }
-    
+
+
 }
 
-nodule.exports = Lobby;
+module.exports = Lobby;
